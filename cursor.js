8b6ea1da3b0107c62811a6efaf5d2be5d6db35bf
MacDudeDude <alexbrhls@gmail.com>
2026-05-07 18:40:11 -0700

    Fixed cursor

--- cursor.js
+++ cursor.js
@@ -13,27 +13,41 @@ const cursorFrames = {
   },
 };
 
+let currentContext = "default";
+let currentFrame = 1;
+
+function applyCursorFrame() {
+  const frames = cursorFrames[currentContext] || cursorFrames.default;
+
+  document.documentElement.style.cursor =
+    currentFrame === 1 ? frames.frame1 : frames.frame2;
+
+  currentFrame = currentFrame === 1 ? 2 : 1;
+}
+
+function setCursorFrames(context = "default") {
+  currentContext = context;
+  applyCursorFrame();
+}
+
+setInterval(applyCursorFrame, 100);
+
+applyCursorFrame();
+
 function detectCursorContext(element) {
   if (element.closest('a, button, input[type="button"], input[type="submit"], input[type="image"], [role="button"], [role="link"], [onclick]')) {
     return 'pointer';
   }
 
-  if (element.closest('label, textarea, input[type="text"], input[type="email"], input[type="password"], input[type="search"], input[type="url"], input[type="tel"], [contenteditable="true"], p, span, h1, h2, h3, h4, h5, h6')) {
+  if (element.closest('label, textarea, input[type="text"], input[type="email"], input[type="password"], input[type="search"], input[type="url"], input[type="tel"], [contenteditable="true"], p, span, h1, h2, h3, h4, h5, h6, p1')) {
     return 'text';
   }
 
   return 'default';
 }
 
-function setCursorFrames(context = 'default') {
-  const frames = cursorFrames[context] || cursorFrames.default;
-  document.documentElement.style.setProperty('--cursor-frame-1', frames.frame1);
-  document.documentElement.style.setProperty('--cursor-frame-2', frames.frame2);
-}
-
 function initCursorSystem() {
   setCursorFrames('default');
-
   document.addEventListener('mouseover', (event) => {
     const context = detectCursorContext(event.target);
     setCursorFrames(context);
